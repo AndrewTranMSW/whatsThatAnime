f34907a9d86137c6772b5cb7dc5ad1a5e4b8c05e
Takara Truong <truont2@gmail.com>
2022-01-13 19:15:46 -0800

    completed search history function

--- develop/script.js
+++ develop/script.js
@@ -82,6 +82,7 @@ function getApi(requestURL) {
 // })
 var storedSearches = [];
 var objValues = [];
+var storedAnime = [];
 var container = document.querySelector(".container");
 var gifContainer = document.querySelector("#gifs");
 // &limit=25&offset=0&rating=pg-13&lang=en
@@ -110,54 +111,43 @@ function fetchTraceAPI(url) {
                 return response.json();
             })
             .then((res) => {
-                console.log(res);
+                // console.log(res);
                 var obj = {};
                 
                 Aniname.innerHTML = res.result[0].anilist.title.romaji;
                 anime = res.result[0].anilist.title.romaji;
-                console.log(anime);
-                // store searched anime name and url
-                obj["name"] = anime;
-                obj["url"] = url;
-                storedSearches.push(obj);
-                console.log(storedSearches);
+
                 pic.appendChild(uploaded_pic);
                 getGiphyApi(anime);
-                console.log(anime);
                 aniSearch(anime);
                 return anime;
             })
             .then((anime) => {
-              if (storedSearches.length === 0) {
-                  // works
-                  console.log("stop1");
-                  var obj = {};
-                  obj["name"] = anime;
-                  obj["url"] = url;
-                  storedSearches.push(obj);
-                  localStorage.setItem('Animes', JSON.stringify(storedSearches));
-                  storage();
-              } else {
-                  // not working
-                  console.log("array has 1 item");
-                  
-                  objValues = Object.values(storedSearches);
-                  console.log(objValues);
+              // this array will have every single anime ever u try to find, bad thinking but need for now
+              
+              console.log(anime + " anime name");
+              console.log(typeof anime)
+              console.log(!storedSearches.includes(anime) + " if array contains text");
+              console.log(typeof storedSearches);
+              console.log(storedSearches);
 
-                  if(!objValues.includes(anime)){
-                      console.log("work if the anime is new");
-                      // store searched anime name and url
-                      // issue, anime name is not being transferred over
-                      var obj = {};
-                      obj["name"] = anime;
-                      obj["url"] = url;
-                      console.log(obj);
-                      storedSearches.push(obj);
-                      localStorage.setItem('Animes', JSON.stringify(storedSearches));
-                      storage();
-                  } 
-              }
-  });
+              if(!storedSearches.includes(anime)){
+                storedSearches.unshift(anime);
+                var obj = {};
+                obj["name"] = anime;
+                obj["url"] = url;
+                console.log(obj);
+                storedAnime.push(obj);
+                console.log(storedAnime);
+                // store searched anime name and url
+                // issue, anime name is not being transferred over
+                localStorage.setItem('Animes', JSON.stringify(storedAnime));
+                localStorage.setItem('Searches', JSON.stringify(storedSearches));
+                storage();  
+              } 
+
+              
+            });
 }
 
 // fetch request for downloaded file
@@ -270,21 +260,22 @@ function readURL(input) {
 
 function aniStats(response) {
     console.log(response)
-    // console.log(response.results[0].synopsis)
+    console.log(response.results[0].synopsis)
 };
 
 var prevSearched = document.querySelector("#list");
 function storage() {
     prevSearched.innerHTML = '';
-    storedSearches = JSON.parse(localStorage.getItem('Animes')) || [];
-    storedSearches.map( item => {
+    storedAnime = JSON.parse(localStorage.getItem('Animes')) || [];
+    storedSearches = JSON.parse(localStorage.getItem('Searches')) || [];
+    storedAnime.map( item => {
         let li = document.createElement('button')
         li.setAttribute('class', 'previous button');
         li.setAttribute("data-value", item["url"]);
         li.textContent = item["name"];
         prevSearched.append(li);
-        objValues = Object.values(storedSearches);
-    })
+        // objValues = Object.values(storedSearches);
+    }) 
 }
 
 prevSearched.addEventListener('click', function(event) {
@@ -294,3 +285,4 @@ prevSearched.addEventListener('click', function(event) {
 });
 
 storage();
+
